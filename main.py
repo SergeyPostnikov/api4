651a56231cbba483fd4f64f04a3024143c79eb3c
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-01 14:10:21 +0300

    working with paths in safe way

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
+import os
 
-
+directory = 'images'
 filename = 'hubble.jpeg'
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-
 response = requests.get(url)
 response.raise_for_status()
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 with open(f'images/{filename}', 'wb') as file:
     file.write(response.content)
