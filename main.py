3e1f97f166ff0c3be849bea191705055807ffc9b
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-01 14:29:02 +0300

    get_picture added

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import requests
 import os
+from urllib.parse import urlparse
 
-directory = 'images'
-filename = 'hubble.jpeg'
-url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-response = requests.get(url)
-response.raise_for_status()
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
+def get_picture(url, directory):
+    filename = urlparse(url).path.split('/')[-1]
+    response = requests.get(url)
+    response.raise_for_status()
 
-with open(f'images/{filename}', 'wb') as file:
-    file.write(response.content)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(f'images/{filename}', 'wb') as file:
+        file.write(response.content)
+
+
+if __name__ == '__main__':
+    get_picture(
+        'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 
+        'images')
