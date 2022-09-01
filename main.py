36d7ec444919674963191d0af60b7278ec278b54
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-01 12:32:53 +0300

    task 2 implemented

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 response = requests.get(url)
 response.raise_for_status()
 
-with open(filename, 'wb') as file:
+with open(f'images/{filename}', 'wb') as file:
     file.write(response.content)
