1231861191a9340d5fdf8a7803310a8dc044fe03
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-09-05 20:40:54 +0300

    get_epic_links implemented

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def get_ext(url):
     return splitext(get_filename(url))[1]
 
 
+def get_date(dt):
+    return "/".join(dt.split()[0].split('-'))
+
+
 def get_picture(url, directory):
     response = requests.get(url)
     response.raise_for_status()
@@ -54,6 +58,21 @@ def fetch_apod():
         get_picture(url, 'images')
 
 
+def get_epic_links():
+    links = []
+    url = "https://api.nasa.gov/EPIC/api/natural/images"
+    payload = {
+        "api_key": "sKrvKPzb8Jma4bOxmW00Q679IyIEGg7CvcRq9YRU",
+    }
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    for resp in response.json():
+        date = get_date(resp.get("date"))
+        filename = f'{resp.get("image")}.png'
+        links.append(f'https://api.nasa.gov/EPIC/archive/natural/{date}/png/{filename}')
+    return links
+
+
 if __name__ == '__main__':
     # get_picture(
     #     'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 
@@ -61,4 +80,5 @@ if __name__ == '__main__':
     # print((get_links()))
     # fetch_spacex_last_launch()
     # print(get_apod_links(2))
-    fetch_apod()
+    # fetch_apod()
+    print(get_epic_paths())
