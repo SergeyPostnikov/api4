ae6b9b5044b69013814523ff39dd6f8322bed5e2
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-09-07 20:30:19 +0300

    fetch_apod_images cli implemented

--- fetch_apod_images.py
+++ fetch_apod_images.py
@@ -1,12 +1,16 @@
 import argparse
 import requests
+import os
+
+from helpers import get_picture
+from dotenv import load_dotenv
 
 
-def get_apod_links(count):
+def get_apod_links(auth_token, count):
     urls = []
     url = "https://api.nasa.gov/planetary/apod"
     payload = {
-        "api_key":  "sKrvKPzb8Jma4bOxmW00Q679IyIEGg7CvcRq9YRU",
+        "api_key":  auth_token,
         "count": count
     }
     response = requests.get(url, params=payload)
@@ -16,6 +20,16 @@ def get_apod_links(count):
     return urls
 
 
-def fetch_apod():
-    for url in get_apod_links(30):
+def fetch_apod(auth_token, count):
+    for url in get_apod_links(auth_token, count):
         get_picture(url, 'images')
+
+
+if __name__ == '__main__':  
+    load_dotenv()
+    auth_token = os.getenv('NASA_API_KEY')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('count', help='number of links')
+    args = parser.parse_args()
+    fetch_apod(auth_token, args.count)
