270b75013d17530cd9bcf79b84c2384de4264682
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-08 17:35:31 +0300

    publish text to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,4 +8,7 @@ if __name__ == '__main__':
     load_dotenv()
     auth_token = os.getenv('TELEGRAM_API_KEY')
     bot = telegram.Bot(token=auth_token)
-    print(bot.get_me())
+    # print(bot.get_me())
+    # updates = bot.get_updates()
+    # print(updates[0])
+    bot.send_message(text="Let's the game begin", chat_id=-1001691456773)
