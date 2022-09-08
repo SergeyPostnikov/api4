8ba24445c71bf1e4aaf68d1af50d8e26a137d11a
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-08 17:06:57 +0300

    keep token in safe way

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,11 @@
 import telegram
+import os
 
-bot = telegram.Bot(token='5799792725:AAESDc0XWs4fXfBv-cVuhCOfRsllaMj9CwI')
-print(bot.get_me())
+from dotenv import load_dotenv
+
+
+if __name__ == '__main__':  
+    load_dotenv()
+    auth_token = os.getenv('TELEGRAM_API_KEY')
+    bot = telegram.Bot(token=auth_token)
+    print(bot.get_me())
