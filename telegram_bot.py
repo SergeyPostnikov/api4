16b7ffeb2628c7bbb3956f9167542dbe2ee57e0f
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-09-08 19:48:37 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,4 +11,10 @@ if __name__ == '__main__':
     # print(bot.get_me())
     # updates = bot.get_updates()
     # print(updates[0])
-    bot.send_message(text="Let's the game begin", chat_id=-1001691456773)
+    chat_id = -1001691456773
+    # bot.send_message(text="Let's the game begin", chat_id=chat_id)
+
+    bot.send_photo(chat_id=chat_id, photo=open('images/BlackHole_Simonnet_960.jpg', 'rb'))
+    # i don't f@@n know where in that docs this method
+    # 'goto' is best way to find something what you need
+    
