4df2fc13c6b430afb1caff0b16282c5127802e97
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-09-08 20:44:03 +0300

    get_random_picture and periodic posting are implemented

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,20 +1,24 @@
 import telegram
 import os
+import time
+import random
 
 from dotenv import load_dotenv
 
 
-if __name__ == '__main__':  
+def get_random_picture(directory):
+    *args, files = next(os.walk(directory))
+    random.shuffle(files)
+    return f'{directory}/{random.choice(files)}'
+
+
+if __name__ == '__main__':
     load_dotenv()
     auth_token = os.getenv('TELEGRAM_API_KEY')
+    period = os.getenv('PUB_PERIOD_HRS')
     bot = telegram.Bot(token=auth_token)
-    # print(bot.get_me())
-    # updates = bot.get_updates()
-    # print(updates[0])
     chat_id = -1001691456773
-    # bot.send_message(text="Let's the game begin", chat_id=chat_id)
 
-    bot.send_photo(chat_id=chat_id, photo=open('images/BlackHole_Simonnet_960.jpg', 'rb'))
-    # i don't f@@n know where in that docs this method
-    # 'goto' is best way to find something what you need
-    
+    while True:
+        bot.send_photo(chat_id=chat_id, photo=open(get_random_picture('images'), 'rb'))
+        time.sleep(3600 * int(period))
