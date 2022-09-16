b76bb23ef2bd68f0af9faaccaf6616bc025c8004
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-09-16 22:31:39 +0300

    KeyError handlers added

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -8,12 +8,16 @@ def get_spacex_links(launch_id):
     url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(url)
     response.raise_for_status()
-    return response.json().get('links').get('flickr').get('original')
+    try:
+        return response.json()['links']['flickr']['original']
+    except KeyError:
+        return []
 
 
-def fetch_spacex_launch(launch_id=None):
+def fetch_spacex_launch(launch_id):
     for url in get_spacex_links(launch_id):
         get_picture(url, 'images')
+        print('fetch')
 
 
 if __name__ == '__main__':
@@ -22,4 +26,4 @@ if __name__ == '__main__':
     parser.add_argument('--launch_id', help='launch id', default='latest')
     args = parser.parse_args()
     fetch_spacex_launch(args.launch_id)
-    print(args.launch_id)
+    print(get_spacex_links(args.launch_id))
