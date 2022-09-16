167fb8b8ddfab715b8bc3c08bbbeb1b3ccaae9ac
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-09-16 21:25:52 +0300

    spacex cli got default

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -4,11 +4,8 @@ import requests
 from picture_handlers import get_picture
 
 
-def get_spacex_links(launch_id=None):
-    if launch_id is not None:
-        url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
-    else:
-        url = 'https://api.spacexdata.com/v5/launches/latest'
+def get_spacex_links(launch_id):
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(url)
     response.raise_for_status()
     return response.json().get('links').get('flickr').get('original')
@@ -22,7 +19,7 @@ def fetch_spacex_launch(launch_id=None):
 if __name__ == '__main__':
     # for test use --launch_id 5eb87d47ffd86e000604b38a
     parser = argparse.ArgumentParser()
-    parser.add_argument('--launch_id', help='launch id')
+    parser.add_argument('--launch_id', help='launch id', default='latest')
     args = parser.parse_args()
     fetch_spacex_launch(args.launch_id)
     print(args.launch_id)
