1ea523248e9fd5ff122712324d8332a0ec805272
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-09-18 20:23:26 +0300

    image naming adjunct by datetime

--- picture_handlers.py
+++ picture_handlers.py
@@ -10,7 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent
 
 
 def get_filename(url):
-    return unquote(urlparse(url).path.split('/')[-1])
+    dt = datetime.now()
+    filename = dt.strftime("%Y-%m-%d-%H-%M-%S")
+    return filename + unquote(urlparse(url).path.split('/')[-1])
 
 
 def get_ext(url):
